30ed259f76ff609c841f4541863c29a1dc25603d
Jophizz <adenijijoseph4@gmail.com>
2024-08-21 06:43:48 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,63 @@
+import { useEffect, useState, useCallback } from "react";
+import { Intro } from './Framework/Screens/Intro';
+import { Pacifico_400Regular } from "@expo-google-fonts/pacifico";
+import {
+  Montserrat_100Thin, Montserrat_200ExtraLight, Montserrat_300Light, Montserrat_400Regular,
+  Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold, Montserrat_800ExtraBold, Montserrat_900Black
+} from "@expo-google-fonts/montserrat"
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+// import { StackNavigator } from "./Framework/Navigators/StackNavigator";
+// import { Preloader } from "./Framework/Components/Preloader";
+// import { AppProvider } from "./global/globalVariables";
+import { RootSiblingParent } from "react-native-root-siblings";
+import { StackNavigator } from "./Framework/Navigation/StackNavigation";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({ Pacifico_400Regular });
+        await Font.loadAsync({ Montserrat_100Thin });
+        await Font.loadAsync({ Montserrat_200ExtraLight });
+        await Font.loadAsync({ Montserrat_300Light });
+        await Font.loadAsync({ Montserrat_400Regular });
+        await Font.loadAsync({ Montserrat_500Medium });
+        await Font.loadAsync({ Montserrat_600SemiBold });
+        await Font.loadAsync({ Montserrat_700Bold });
+        await Font.loadAsync({ Montserrat_800ExtraBold });
+        await Font.loadAsync({ Montserrat_900Black });
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <RootSiblingParent>
+      <NavigationContainer>
+      
+      <StackNavigator />
+
+      </NavigationContainer>
+    </RootSiblingParent>
+  );
+}
---
+++ Framework/Navigation/StackNavigation.js
@@ -0,0 +1,19 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../Screens/HomeScreen';
+import { Intro } from '../Screens/Intro';
+import { Login } from '../Screens/Login';
+import { Profile } from '../Screens/Profile';
+import {PostAdForm} from '../Screens/PostAdForm';
+const Stack = createStackNavigator();
+
+export function StackNavigator() {
+    return (
+        <Stack.Navigator initialRouteName="Intro">
+            <Stack.Screen name="Intro" component={Intro} options={{ headerShown: false }} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Profile" component={Profile} />
+            <Stack.Screen name="PostAdForm" component={PostAdForm}/>
+        </Stack.Navigator>
+    );
+}
